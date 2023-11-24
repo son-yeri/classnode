35cfff798c83049cd02baa8f5daaf35358cd356e
son-yeri <80444222+son-yeri@users.noreply.github.com>
2023-11-24 12:54:28 +0900

    Add files via upload

---
+++ secver.js
@@ -0,0 +1,10 @@
+const express = require('exprsee');
+const app = express();
+
+app.listen(8080, fucyion(){
+    console.log("포토 8080으로 서버 대기중...")
+})"";
+
+app.get('./book', function(req, res){
+    res.send('도서 페이지 입니다.');
+})
